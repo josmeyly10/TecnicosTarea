6a942b0d538f35bbbfce4dcd5dc0bf0b9c39ba9c
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2025-01-18 10:45:49 -0400

    aqui se inyectaron el contexto y el servicio

---
+++ RegistroTecnicos/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using RegistroTecnicos.Components;
+using RegistroTecnicos.DAL;
+using RegistroTecnicos.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// inyeccion del contexto 
+
+var ConStr = builder.Configuration.GetConnectionString("ConStr"); 
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConStr));
+
+//Inyeccion del service
+builder.Services.AddScoped<TecnicosService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
