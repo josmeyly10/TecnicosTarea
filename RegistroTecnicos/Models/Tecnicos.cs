5c951e0f719597c5102c7e0ed104b2462e496c2a
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2025-01-18 10:41:57 -0400

    aqui se hizo el modelo del tecnicos

---
+++ RegistroTecnicos/Models/Tecnicos.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class Tecnicos
+    {
+        [Key]
+        public int TecnicosId { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z]+$",ErrorMessage = "solo se permiten letras ")]
+        [Required(ErrorMessage ="Campo nombres obligatorios")]
+
+        public string? Nombres { get; set; }
+
+        [Range(0.01,double.MaxValue ,ErrorMessage = "ingrese un valor mayor a 0")]
+        [Required(ErrorMessage = "Campo sueldo obligatorio")]
+
+        public double SueldoHoras { get; set; }
+    }
+}
+ 
