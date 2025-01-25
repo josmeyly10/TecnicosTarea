3e167a5388e48af806cbed97ed48c2017e4f98e7
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2025-01-25 00:09:29 -0400

    se hicieron las migraciones

---
+++ RegistroTecnicos/Migrations/20250122014213_Initial.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroTecnicos.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tecnicos",
+                columns: table => new
+                {
+                    TecnicosId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombres = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SueldoHoras = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tecnicos", x => x.TecnicosId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    ClientesId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombres = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaIngreso = table.Column<string>(type: "nvarchar(6)", maxLength: 6, nullable: false),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rnc = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    LimiteCredito = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TecnicosId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.ClientesId);
+                    table.ForeignKey(
+                        name: "FK_Clientes_Tecnicos_TecnicosId",
+                        column: x => x.TecnicosId,
+                        principalTable: "Tecnicos",
+                        principalColumn: "TecnicosId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clientes_TecnicosId",
+                table: "Clientes",
+                column: "TecnicosId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Tecnicos");
+        }
+    }
+}
